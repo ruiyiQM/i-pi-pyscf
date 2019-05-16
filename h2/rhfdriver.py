439fb2eff562a6337c00c7f2753dad2c4a498938
Ruiyi Zhou <zry3035140330@gmail.com>
2019-05-16 18:01:44 +0800

    Add files via upload

---
+++ h2/rhfdriver.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("../../")
+from driver import RHFDriver, ExitSignal, TimeOutSignal
+
+driver = RHFDriver(31415, "127.0.0.1", "template.gjf", ["H", "H"],'sto-3g')
+while True:
+    try:
+        driver.parse()
+    except ExitSignal as e:
+        driver = RHFDriver(31415, "127.0.0.1", "template.gjf", ["H", "H"],'sto-3g')
+    except TimeOutSignal as e:
+        print("Time out. Check whether the server is closed.")
+        exit()
